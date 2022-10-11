c0742f388d125c2ea4cf89a4fa75a729369d0bff
lett39 <lettaw30@gmail.com>
2022-10-11 15:41:15 +0000

    rcav updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,24 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"}) 
+  end   
 
   def play_rock
      #@some_number = rand(1000)
 
      #redirect_to (URL's)
-     redirect_to("https://www.playrock.com")
+     #redirect_to("https://www.playrock.com")
 
      render({ :template => "game_template/rock.html.erb"})
-
-     #render({ :template => "game_template/paper.html.erb"})
-
-     #render({ :template => "game_template/scissors.html.erb"})
-     
   end
-end
+
+  def play_paper   
+     render({ :template => "game_template/paper.html.erb"})
+  end
+
+  def play_scissors
+     render({ :template => "game_template/scissors.html.erb"})
+  end   
+ end
