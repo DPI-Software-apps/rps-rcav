a883f317896f7dce6b22d87b179ef2ebd388f320
lett39 <lettaw30@gmail.com>
2022-10-11 17:23:30 +0000

    rcav updates5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,30 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+  #@some_number = rand(1000)
+
+     #redirect_to (URL's)
+     #redirect_to("https://www.playrock.com")
+
   def homepage
     render({ :template => "game_template/rules.html.erb"}) 
   end   
 
   def play_rock
-     #@some_number = rand(1000)
+     @comp_move = ["rock","paper","scissors"].sample
 
-     #redirect_to (URL's)
-     #redirect_to("https://www.playrock.com")
 
-     render({ :template => "game_template/rock.html.erb"})
+   if @comp_move == "paper" 
+     @outcome = "tied"
+   elsif @comp_move == "rock"
+     @outcome = "lost"
+   elsif @comp_move == "scissors"
+     @outcome = "won"
+   end 
+
+     render({ :template => "game_template/rock.html.erb" :layout =>"wrapper.html.erb" })
   end
 
   def play_paper 
@@ -28,10 +39,20 @@ class ApplicationController < ActionController::Base
      @outcome = "won"
    end 
    
-     render({ :template => "game_template/paper.html.erb"})
+     render({ :template => "game_template/paper.html.erb" :layout =>"wrapper.html.erb" })
   end
 
   def play_scissors
-     render({ :template => "game_template/scissors.html.erb"})
+   @comp_move = ["rock","paper","scissors"].sample
+
+
+   if @comp_move == "paper" 
+     @outcome = "tied"
+   elsif @comp_move == "rock"
+     @outcome = "lost"
+   elsif @comp_move == "scissors"
+     @outcome = "won"
+   end 
+     render({ :template => "game_template/scissors.html.erb" :layout =>"wrapper.html.erb" })
   end   
  end
